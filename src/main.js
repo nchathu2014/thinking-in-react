ca72910e09f396335a82a3e59abae527af9e3d44
Chathuranga <UCHATNU@PEROOT.COM>
2016-02-09 20:21:25 +0530

    Intial commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ToDoListProvider from './components/ToDoListProvider';
+
+ReactDOM.render(
+	<ToDoListProvider/>,
+	document.getElementById('container')
+);
+
+
+/*ReactDOM.render(
+	<ToDoList/>,
+	document.getElementById('container')
+);*/
+
+
+
